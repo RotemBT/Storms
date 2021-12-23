a0ea285ba1e556bdced412f05b1203996a22fd6a
sapir daga <sapirddd001@gmail.com>
2021-12-23 22:16:37 +0200

    DataFrame of one url

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 # Import libraries
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 from bs4 import BeautifulSoup
+import pandas as pd
 
 
 def crawling_table():
     # url of the web
     url = "https://www.wunderground.com/hurricane/archive/WP"
+
+    """
+    # urls of 6 oceans
+    all_url =['https://www.wunderground.com/hurricane/archive/AL',"https://www.wunderground.com/hurricane/archive/EP","https://www.wunderground.com/hurricane/archive/WP",
+          "https://www.wunderground.com/hurricane/archive/IO","https://www.wunderground.com/hurricane/archive/CP","https://www.wunderground.com/hurricane/archive/SH"]
+    """
 
     # path of chrome_driver at specific computer
     s = Service('C:/Users/sapir/Documents/Storms/chromedriver.exe')
@@ -17,29 +21,34 @@ def crawling_table():
     # web_driver object
     driver = webdriver.Chrome(service=s)
     driver.get(url)
+
     c = driver.page_source
     bs = BeautifulSoup(c, "html.parser")
-    table = bs.find_all('div', {'class': "storms-wrapper mat-elevation-z8 ng-star-inserted"})
 
     years = []
     storms = []
     hurricanes = []
     deaths = []
     damanged_usd = []
+    tdOfYear = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-year mat-column-year ng-star-inserted'})
+    tdOfStorm = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-storms mat-column-storms ng-star-inserted'})
+    tdOHurricanes = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-hurricanes mat-column-hurricanes ng-star-inserted'})
+    tdOfdeaths = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-deaths mat-column-deaths ng-star-inserted'})
+    tdOfdamanged_usd = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-damage mat-column-damage ng-star-inserted'})
 
-    # Find elements in table
-    table_elements = WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.XPATH, "//table[@class = 'mat-table cdk-table mat-sort']")))
-    for table_element in table_elements:
-        for row in table_element.find_elements(By.XPATH,".//tr"):
-            print(row.text)
-    """
-    for t in table:
-        print(driver.find_elements(By.CLASS_NAME,"storms-wrapper mat-elevation-z8 ng-star-inserted"))
-       # x=t.find_all("tr").find_all("td").text
-       # years.append(x)
-    years
-    """
-
+    for td in tdOfYear:
+        years.append(td.find('a').text)
+    for td in tdOfStorm:
+        storms.append(td.find('span').text)
+    for td in tdOHurricanes:
+        hurricanes.append(td.find('span').text)
+    for td in tdOfdeaths:
+        deaths.append(td.find('span').text)
+    for td in tdOfdamanged_usd:
+        damanged_usd.append(td.find('span').text)
+    d={'years':years,'storms':storms,'hurricanes':hurricanes,'death':deaths,'damanged_usd':damanged_usd}
+    df = pd.DataFrame(data=d)
+    print(df)
     driver.close()
 
 
