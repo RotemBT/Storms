35fa32a08c623060e1c0beafa4bae80ad8dbbb7f
sapir daga <sapirddd001@gmail.com>
2021-12-24 21:07:19 +0200

    DataFrame to csv - all tables of first page (from all oceans)

--- main.py
+++ main.py
@@ -7,49 +7,56 @@ import pandas as pd
 
 def crawling_table():
     # url of the web
-    url = "https://www.wunderground.com/hurricane/archive/WP"
+    # url = "https://www.wunderground.com/hurricane/archive/WP"
 
-    """
     # urls of 6 oceans
-    all_url =['https://www.wunderground.com/hurricane/archive/AL',"https://www.wunderground.com/hurricane/archive/EP","https://www.wunderground.com/hurricane/archive/WP",
-          "https://www.wunderground.com/hurricane/archive/IO","https://www.wunderground.com/hurricane/archive/CP","https://www.wunderground.com/hurricane/archive/SH"]
-    """
+    all_url = ['https://www.wunderground.com/hurricane/archive/AL', "https://www.wunderground.com/hurricane/archive/EP",
+               "https://www.wunderground.com/hurricane/archive/WP",
+               "https://www.wunderground.com/hurricane/archive/IO", "https://www.wunderground.com/hurricane/archive/CP",
+               "https://www.wunderground.com/hurricane/archive/SH"]
 
     # path of chrome_driver at specific computer
     s = Service('C:/Users/sapir/Documents/Storms/chromedriver.exe')
-
-    # web_driver object
-    driver = webdriver.Chrome(service=s)
-    driver.get(url)
-
-    c = driver.page_source
-    bs = BeautifulSoup(c, "html.parser")
-
     years = []
     storms = []
+    storms_names = []
     hurricanes = []
     deaths = []
     damanged_usd = []
-    tdOfYear = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-year mat-column-year ng-star-inserted'})
-    tdOfStorm = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-storms mat-column-storms ng-star-inserted'})
-    tdOHurricanes = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-hurricanes mat-column-hurricanes ng-star-inserted'})
-    tdOfdeaths = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-deaths mat-column-deaths ng-star-inserted'})
-    tdOfdamanged_usd = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-damage mat-column-damage ng-star-inserted'})
+    for url in all_url:
 
-    for td in tdOfYear:
-        years.append(td.find('a').text)
-    for td in tdOfStorm:
-        storms.append(td.find('span').text)
-    for td in tdOHurricanes:
-        hurricanes.append(td.find('span').text)
-    for td in tdOfdeaths:
-        deaths.append(td.find('span').text)
-    for td in tdOfdamanged_usd:
-        damanged_usd.append(td.find('span').text)
-    d={'years':years,'storms':storms,'hurricanes':hurricanes,'death':deaths,'damanged_usd':damanged_usd}
+        driver = webdriver.Chrome(service=s)
+        driver.get(url)
+
+        c = driver.page_source
+        bs = BeautifulSoup(c, "html.parser")
+
+        tdOfYear = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-year mat-column-year ng-star-inserted'})
+        tdOfStorm = bs.find_all('td',
+                                {'class': 'mat-cell cdk-cell cdk-column-storms mat-column-storms ng-star-inserted'})
+        tdOHurricanes = bs.find_all('td', {
+            'class': 'mat-cell cdk-cell cdk-column-hurricanes mat-column-hurricanes ng-star-inserted'})
+        tdOfDeaths = bs.find_all('td',
+                                 {'class': 'mat-cell cdk-cell cdk-column-deaths mat-column-deaths ng-star-inserted'})
+        tdOfDamanged_usd = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-damage mat-column-damage ng-star-inserted'})
+
+        for td in tdOfYear:
+            years.append(td.find('a').text)
+            # storms_names.append(td.find('a')['href'])
+        for td in tdOfStorm:
+            storms.append(td.find('span').text)
+        for td in tdOHurricanes:
+            hurricanes.append(td.find('span').text)
+        for td in tdOfDeaths:
+            deaths.append(td.find('span').text)
+        for td in tdOfDamanged_usd:
+            damanged_usd.append(td.find('span').text)
+
+        driver.close()
+    d = {'years': years, 'storms': storms, 'hurricanes': hurricanes, 'death': deaths, 'damanged_usd': damanged_usd}
     df = pd.DataFrame(data=d)
     print(df)
-    driver.close()
+    df.to_csv('storms_df.csv')
 
 
 if __name__ == '__main__':
