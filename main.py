b4be58a3e064db6a196d99780d8dc1f156a2b676
sapir daga <sapirddd001@gmail.com>
2021-12-29 21:32:28 +0200

    scraping next page

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup
 import pandas as pd
-
+from selenium.webdriver.common.by import By
 
 def crawling_table():
     # url of the web
@@ -19,30 +19,47 @@ def crawling_table():
     s = Service('C:/Users/sapir/Documents/Storms/chromedriver.exe')
     years = []
     storms = []
+    maxWinds = []
+    minPressure = []
+    maxStrength = []
     storms_names = []
     hurricanes = []
     deaths = []
     damanged_usd = []
     for url in all_url:
-
         driver = webdriver.Chrome(service=s)
         driver.get(url)
-
         c = driver.page_source
         bs = BeautifulSoup(c, "html.parser")
 
         tdOfYear = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-year mat-column-year ng-star-inserted'})
-        tdOfStorm = bs.find_all('td',
-                                {'class': 'mat-cell cdk-cell cdk-column-storms mat-column-storms ng-star-inserted'})
-        tdOHurricanes = bs.find_all('td', {
-            'class': 'mat-cell cdk-cell cdk-column-hurricanes mat-column-hurricanes ng-star-inserted'})
-        tdOfDeaths = bs.find_all('td',
-                                 {'class': 'mat-cell cdk-cell cdk-column-deaths mat-column-deaths ng-star-inserted'})
+        tdOfStorm = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-storms mat-column-storms ng-star-inserted'})
+        tdOHurricanes = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-hurricanes mat-column-hurricanes ng-star-inserted'})
+        tdOfDeaths = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-deaths mat-column-deaths ng-star-inserted'})
         tdOfDamanged_usd = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-damage mat-column-damage ng-star-inserted'})
 
         for td in tdOfYear:
             years.append(td.find('a').text)
-            # storms_names.append(td.find('a')['href'])
+            next_page = url + "/" + td.find('a').text
+
+            # update path of next page
+            driver.get(next_page)
+            c = driver.page_source
+            bs = BeautifulSoup(c, "html.parser")
+
+            tdStormsNames = bs.find_all('td',{'class':'mat-cell cdk-cell cdk-column-summaryStormName mat-column-summaryStormName ng-star-inserted'})
+            toOfMaxWinds = bs.find_all('td', {'class': 'mat-cell cdk-cell cdk-column-highestMaximumSustainedWind mat-column-highestMaximumSustainedWind ng-star-inserted'})
+            tdOfMinPressure = bs.find_all('td',{'class':'mat-cell cdk-cell cdk-column-lowestMinimumPressure mat-column-lowestMinimumPressure ng-star-inserted'})
+            tdOfMaxStrength = bs.find_all('td',{'class':'mat-cell cdk-cell cdk-column-maximumStormType mat-column-maximumStormType ng-star-inserted'})
+            for i in tdStormsNames:
+                storms_names.append(i.find('a').text)
+            for i in toOfMaxWinds:
+                maxWinds.append(int(i.find('span').text))
+            for i in tdOfMinPressure:
+                minPressure.append(int(i.find('span').text))
+            for i in tdOfMaxStrength:
+                maxStrength.append(i.text)
+
         for td in tdOfStorm:
             storms.append(td.find('span').text)
         for td in tdOHurricanes:
@@ -53,7 +70,8 @@ def crawling_table():
             damanged_usd.append(td.find('span').text)
 
         driver.close()
-    d = {'years': years, 'storms': storms, 'hurricanes': hurricanes, 'death': deaths, 'damanged_usd': damanged_usd}
+    d = {'years': years, 'storms': storms, 'hurricanes': hurricanes, 'death': deaths, 'damanged_usd': damanged_usd,
+         }
     df = pd.DataFrame(data=d)
     print(df)
     df.to_csv('storms_df.csv')
