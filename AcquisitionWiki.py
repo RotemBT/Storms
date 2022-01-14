0ca90daa2a09773ccfa63af2ead7cc98c56d48ff
Rotem <rotem001@gmail.com>
2022-01-14 12:47:58 +0200

    refactor dataframes

--- AcquisitionWiki.py
+++ AcquisitionWiki.py
@@ -1,4 +1,6 @@
-from AcquisitionHelper import *
+from AcquisitionHelper import scrapFromWiki,urlToScrap,FIRST_YEAR,LAST_YEAR
+import pandas as pd
+import time
 
 start = time.time()
 stormName = []
@@ -21,7 +23,7 @@ dataset = pd.DataFrame(
     {'Storm Name': stormName, 'Year': year, 'Dates Active': datesActive, 'Storm Category': stormCategory
         , 'Max min wind': maxMinWind, 'Min Pressures': minPressure
         , 'Damage USD': damageUSD, 'Deaths': deaths})
-
+dataset.to_csv('wiki.csv')
 print(dataset)
 
 print(f"This program take {time.time() - start} seconds")
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from AcquisitionHelper import *
 
 yearOfStorm = []
@@ -12,8 +14,10 @@ windPower = []
 airPressure = []
 deaths = []
 damagedUsd = []
+
 scrapGeneralInformationOfOcean(oceansURL)
 
 for i in range(2021, 1851, -1):
     scrapDataFromCurrYear(i, 'Atlantic Ocean', yearOfStorm, oceans, dates, hours, windPower, airPressure,
                           stormType, stormsName, latCorr, longCorr)
+
