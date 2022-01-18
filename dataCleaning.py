414e930e146acf0e2168c76abce2b658966efef8
Rotem <rotem001@gmail.com>
2022-01-18 16:32:24 +0200

    add formula to wind speed

--- dataCleaning.py
+++ dataCleaning.py
@@ -15,15 +15,28 @@ print(indexNames)
 print(df.iloc[indexNames])
 print(df.duplicated().sum())
 print(removeDuplicatives(df))
-print(df.dropna(thresh=len(df.columns)-2))
-def squareEqution(a,b,c):
+print(df.dropna(thresh=len(df.columns) - 2))
+
+
+def windSpeedToPressure(windSpeedInMPH):
+    windSpeedInMS = windSpeedInMPH * 0.44704
+    return round(1014.9 - 0.361451 * windSpeedInMS - 0.00259 * windSpeedInMS ** 2)
+
+
+def pressureToWindSpeed(pressure):
     # https://sciencing.com/convert-wind-speed-pressure-5814125.html
     # convertor -https://www.metric-conversions.org/speed/miles-per-hour-to-meters-per-second.htm
+    a=-0.00259
+    b=-0.361451
+    c=1014.9-pressure
     try:
-        d = (b**2) -(4*a*c)
-        sol1 = (-b-math.sqrt(d))/(2*a)
-        sol2 = (-b+math.sqrt(d))/(2*a)
-        print("Solution are {0} and {1}".format(sol1,sol2))
+        d = (b ** 2) - (4 * a * c)
+        sol1 = (-b - math.sqrt(d)) / (2 * a)
+        sol2 = (-b + math.sqrt(d)) / (2 * a)
+        return round(max(sol1, sol2))
     except:
         return None
-squareEqution(-0.00259,-0.31451,1020-1008)
+
+
+print(pressureToWindSpeed(985))
+print(windSpeedToPressure(52))
