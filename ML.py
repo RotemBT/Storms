411132aaae3ec028ddd39794577c18903d1513a1
Rotem <rotem001@gmail.com>
2022-01-22 17:27:25 +0200

    adding date

--- ML.py
+++ ML.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pandas as pd
 import sklearn
 from sklearn.linear_model import LinearRegression
@@ -11,10 +13,16 @@ labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 df['beaufort_scale'] = pd.cut(df['wind_power'], bins, labels=labels)
 df['Ocean'] = df['Ocean'].astype('category')
 df['ocean_code'] = df['Ocean'].cat.codes
-print(df['beaufort_scale'].max())
-print(df['beaufort_scale'].min())
-X = df[df.columns[
-    (df.columns != 'Ocean') & (df.columns != 'storm_name') & (df.columns != 'time') & (df.columns != 'storm_type')]]
+
+df['curr_date'] = df.apply(
+    lambda row: datetime.datetime.strptime(f"{int(row.year)}-{int(row.Month)}-{int(row.Day)}", '%Y-%m-%d'), axis=1)
+df["curr_date"] = df["curr_date"].dt.strftime('%Y%m%d').astype(float)
+print(df['curr_date'])
+"""X = df[df.columns[
+    (df.columns == 'curr_date') & (df.columns == 'lat') & (df.columns == 'long') & (df.columns == 'air_pressure') & (
+            df.columns == 'beaufort_scale')]]"""
+X = df.loc[:, ~df.columns.isin(['storm_name', 'year', 'time', 'wind_power', 'storm_type', 'Ocean', 'Month', 'Day'])]
 y = df['wind_power']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=40)
-model = sklearn.linear_model.LinearRegression().fit(X_train, y_train)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+model = LinearRegression().fit(X_train, y_train)
+
