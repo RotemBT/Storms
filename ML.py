2cd0580ae91290a8503e3e921a83d1a10362a6c7
Rotem <rotem001@gmail.com>
2022-01-22 22:40:09 +0200

    complete model

--- ML.py
+++ ML.py
@@ -10,6 +10,36 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 
 df = pd.DataFrame(pd.read_csv('addingBeaufort.csv'))
+scaler = MinMaxScaler(feature_range=(0, 1))
+std = StandardScaler()
+X = df.loc[:, ~df.columns.isin(
+    ['storm_name', 'time', 'wind_power', 'storm_type', 'Ocean','ocean_code'
+     ,'curr_date','beaufort_scale'])]
+y = df['wind_power']
+print(X)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0, shuffle=False)
+model = LinearRegression().fit(X_train, y_train)
+y_pred = model.predict(X_test)
+print(r2_score(y_test, y_pred))
+print(model.predict([[1951, 980, 20, 105, 10, 3]]))
+"""
+lat = float(input("Please enter Lat value: "))
+long = float(input("Please enter Long value: "))
+date_entry = float(input('Enter a date in Month format'))
+year, month, day = map(int, date_entry.split('-'))
+date = float(datetime.date(year, month, day).strftime('%Y%m%d'))
+
+while (-90 <= lat <= 90 and -180 <= long <= 180):
+    userInput = [lat, long, date_entry]
+    # userInput=scaler.fit_transform(userInput)
+    print(
+        'Prediction of wind power is : {:.1f} mph'.format(round(float(model.predict([[lat, long, date_entry]])[0]))))
+    lat = float(input("Please enter Lat value: "))
+    long = float(input("Please enter Long value: "))
+    date_entry = float(input('Enter a date in Month format'))
+
+print('Input is out of range!')
+"""
 """
 bins = [0, 1, 3, 7, 12, 18, 24, 31, 38, 46, 54, 63, 72, 250]
 labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
@@ -22,38 +52,3 @@ df['curr_date'] = df.apply(
 df["curr_date"] = df["curr_date"].dt.strftime('%Y%m%d').astype(float)
 df.drop('Unnamed: 0', 1,inplace=True)
 df.to_csv('addingBeaufort.csv', index=False)"""
-# print(df['curr_date'])
-scaler = MinMaxScaler(feature_range=(-1, 1))
-std = StandardScaler()
-X = df.loc[:, ~df.columns.isin(
-    ['ocean_code', 'storm_name', 'year', 'time', 'wind_power', 'storm_type', 'Ocean', 'Day', 'air_pressure', 'Month',
-     'beaufort_scale'])]
-print(df.describe())
-print(X.columns)
-y = df['wind_power']
-X= scaler.fit_transform(X)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0, shuffle=False)
-
-model = LinearRegression().fit(X_train, y_train)
-print(model.score(X_test, y_test))
-predicted = model.predict(X_test)
-predicted = np.round(predicted)
-print(predicted)
-print(r2_score(y_test, predicted))
-
-lat = float(input("Please enter Lat value: "))
-long = float(input("Please enter Long value: "))
-date_entry = float(input('Enter a date in Month format'))
-"""year, month, day = map(int, date_entry.split('-'))
-date = float(datetime.date(year, month, day).strftime('%Y%m%d'))"""
-
-while (-90 <= lat <= 90 and -180 <= long <= 180):
-    userInput = [lat, long, date_entry]
-    # userInput=scaler.fit_transform(userInput)
-    print(
-        'Prediction of wind power is : {:.1f} mph'.format(round(float(model.predict([[lat, long, date_entry]])[0]))))
-    lat = float(input("Please enter Lat value: "))
-    long = float(input("Please enter Long value: "))
-    date_entry = float(input('Enter a date in Month format'))
-
-print('Input is out of range!')
